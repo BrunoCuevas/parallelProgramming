eb2366a15657ca0cbb855cf9b9ad33967e47d22a
BrunoCuevas <b.cuevas@alumnos.upm.es>
2016-10-07 10:53:57 +0200

    dynamic memory allocation

--- code/task1.c
+++ code/task1.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 //#define X 10000
 //#define T 10000
 //static double U[X+1][T+2];
  // declare matrix U
-	
+
 int main(int argc, char *argv[]) {
 	int X = atoi(argv[1]);
 	int T = atoi(argv[2]);
-	double **U ;
-	*U = (double *) malloc(sizeof(double)*(X+1)*(T+2));
+	int sizeNumber = (X+1)*(T+2);
+	printf("size of the vector = %d \n", sizeNumber);
+	double *U = malloc(sizeof(double)*(X+1)*(T+3));
 	//double U = puntero;
 	int startTime = clock();
 	int t, x;
 	double L= 0.345678, S;
 	// initialize positions of matrix U
 	for (x=1; x<X; x++) {
-		U[x][0] = sin(x*M_PI/(double)X);
-		U[x][1] = U[x][0] * cos(M_PI /(double)T);
+		U[x] = sin(x*M_PI/(double)X);
+		U[x + X + 1] = U[x] * cos(M_PI /(double)T);
 	}
 	for (t=0; t<=T; t++) {
-		U[0][t] = U[X][t] = 0.0;
+		U[t*(X+1)] = U[t*(X+1) + X] = 0.0;
 	}
+	printf("Hola!\n");
 	// Program Body
 		// tS : timeStep
 		// iX : iterX
@@ -30,23 +33,30 @@ int main(int argc, char *argv[]) {
 		// sndTerm : Second term of the equation
 		// trdTerm : Third term of the equation
 		// fthTerm : Fourth term of the equation
-	int tS, iX;
-	float fstTerm, sndTerm, trdTerm, fthTerm;
+	int tS, iX, address;
+	double fstTerm, sndTerm, trdTerm, fthTerm;
+	printf("Hola\n");
 	for (tS = 1; tS < T + 2; tS ++) {
 		for (iX = 1; iX < X ; iX ++) {
-			fstTerm = 2*(1-L)*U[iX][tS];
-			sndTerm = L*U[iX + 1][tS];
-			trdTerm = L*U[iX - 1][tS];
-			fthTerm = -U[iX][tS - 1];
-			U[iX][tS +1] = fstTerm + sndTerm + trdTerm + fthTerm ;
+			address = (tS*(X+1)) + iX;
+			fstTerm = 2*(1-L)*U[address];
+			sndTerm = L*U[address + 1];
+			trdTerm = L*U[address - 1];
+			fthTerm = -U[address - (X+1)];
+			if (tS > T) {
+				printf("tS =%d \t iX =%d \t address = %d \n", tS, iX, address);
+				printf("nex term = %d\n", address + X + 1);
+			}
+			U[address + (X+1)] = fstTerm + sndTerm + trdTerm + fthTerm ;
 		}
 	}
+	printf("Hola!\n");
 	// obtain checksum of final state
 		// cS : checkSum for a given value
 	S = 0;
+	int fP = (T+1)*(X+1);
 	for (iX = 0; iX <= X; iX ++){
-		
-		S = S + U[iX][T + 2];
+		S = S + U[fP + iX];
 	}
 	printf("CheckSum = %e\n", S);
 	int endTime = clock();
